b2e415ab5e0ad105c5ec84d6514ead79f1e4617d
Sahil Srivastava <sahil682002@gmail.com>
2024-10-22 10:26:19 +0530

    first commit

---
+++ qrcodepractice/script.js
@@ -0,0 +1,14 @@
+let qrinput = document.querySelector(".qrinp");
+let qrimg = document.querySelector(".qrimg");
+let qrbtn = document.querySelector("button");
+
+// https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=
+
+
+function generateQR() {
+
+    if (qrinput.value.length > 0) {
+        let qrurl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrinput.value}`;
+        qrimg.src = qrurl;
+    }   
+}
